aa29bd5fd59dd5ed9e691983d7f263220f18bbb9
Prakrit Shrivastava <shrivastavaprakrit@gmail.com>
2026-06-25 20:22:11 +0530

    added response message

--- tests/api/amadeus.oauth2.spec.ts
+++ tests/api/amadeus.oauth2.spec.ts
@@ -10,46 +10,46 @@ let OAUTH_CONFIG={
 }
 
 //Token might be expired hence always use beforeEach and run it everytime
-test.beforeEach('POST generate token',async({ request })=>{
-    console.log("Running AMADEUS API Test");
-    let response=await request.post(OAUTH_CONFIG.tokenURL,{
-        form:{
-            grant_type:OAUTH_CONFIG.grantType,
-            client_id:OAUTH_CONFIG.clientId,
-            client_secret:OAUTH_CONFIG.clientSecret
-        }
-    });
+// test.beforeEach('POST generate token',async({ request })=>{
+//     console.log("Running AMADEUS API Test");
+//     let response=await request.post(OAUTH_CONFIG.tokenURL,{
+//         form:{
+//             grant_type:OAUTH_CONFIG.grantType,
+//             client_id:OAUTH_CONFIG.clientId,
+//             client_secret:OAUTH_CONFIG.clientSecret
+//         }
+//     });
 
-    //expect(response.status()).toBe(200);
-    let json_res=await response.json();
-    console.log(json_res);
-    accessToken=json_res.accessToken;
+//     //expect(response.status()).toBe(200);
+//     let json_res=await response.json();
+//     console.log(json_res);
+//     accessToken=json_res.accessToken;
 
-});
+// });
 
-test('GET Location Data',async({request})=>{
+// test('GET Location Data',async({request})=>{
 
-    //https://test.api.amadeus.com/v1/reference-data/locations?pubType=CITY,AIRPORT&keyword=MUC&countryCode=DE
-    let baseURL = 'https://test.api.amadeus.com';
-    let endPointURI = '/v1/reference-data/locations';
+//     //https://test.api.amadeus.com/v1/reference-data/locations?pubType=CITY,AIRPORT&keyword=MUC&countryCode=DE
+//     let baseURL = 'https://test.api.amadeus.com';
+//     let endPointURI = '/v1/reference-data/locations';
 
-    let queryParam={
-        subType:'CITY,AIRPORT',
-        keyword:'MUC',
-        countryCode:'DE'
-    };
+//     let queryParam={
+//         subType:'CITY,AIRPORT',
+//         keyword:'MUC',
+//         countryCode:'DE'
+//     };
 
-    let location_json=await request.get(`${baseURL}${endPointURI}`,{
-        headers:{
-           Authorization:`Bearer ${accessToken}`     
-        },
-        params:queryParam
-    });
+//     let location_json=await request.get(`${baseURL}${endPointURI}`,{
+//         headers:{
+//            Authorization:`Bearer ${accessToken}`     
+//         },
+//         params:queryParam
+//     });
 
-    let locationResponse=await location_json.json();
-    console.log("Location: ",locationResponse);
+//     let locationResponse=await location_json.json();
+//     console.log("Location: ",locationResponse);
 
-    let location_one=locationResponse.data[1];
-    console.log("Location: One",location_one);
+//     let location_one=locationResponse.data[1];
+//     console.log("Location: One",location_one);
 
-})
+// })
